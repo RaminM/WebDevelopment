f4269bc21a53c82391383c23829d2e0a99120413
Seyedramin Mortazavi <50069617+RaminM@users.noreply.github.com>
2023-08-16 18:18:30 +0200

    update

--- Newsletter/app.js
+++ Newsletter/app.js
@@ -4,11 +4,17 @@ const request = require("request");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/",function(req,res){
     res.sendFile(__dirname + "/signup.html");
 });
-
+app.post("/",function(req,res){
+    console.log(req.body.fname);
+    console.log(req.body.lname);
+    console.log(req.body.uemail);
+    // console.log(req);
+});
 app.listen(3000,function(){
     console.log("Server is running on port 3000");
 });
